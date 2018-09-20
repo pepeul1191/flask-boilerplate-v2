d294112bc0ee8e1ff84cc22f6d86d46985ba5363
Pepe Valdivia <jvaldivia@softweb.pe>
2018-09-20 18:10:23 -0500

    Comenzando con accesos

--- home/views.py
+++ home/views.py
@@ -9,7 +9,7 @@ home_view = Blueprint('home_view', __name__)
 @home_view.route('/', methods=['GET'])
 @session_true
 def index():
-  return 'home'
+  return '<a href="' + constants['base_url'] + 'accesos/">Ir a Accesos</a>'
 
 @home_view.route('/x', methods=['GET'])
 def xd():
